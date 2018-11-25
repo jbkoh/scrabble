6d64be2e9c7d0392332592c804eb15c20a3e2516
Jason Koh <bk7749@gmail.com>
2018-11-25 03:42:56 +0000

    relax schema condition

--- scrabble/data_model.py
+++ scrabble/data_model.py
@@ -17,6 +17,7 @@ class RawMetadata(Document):
     srcid = StringField(required=True)
     building = StringField(required=True)
     metadata = DictField()
+    meta = {'strict': False}
 
 class LabeledMetadata(Document):
     srcid = StringField(required=True)
@@ -24,6 +25,7 @@ class LabeledMetadata(Document):
     fullparsing = DictField()
     tagsets = ListField(StringField())
     point_tagset = StringField()
+    meta = {'strict': False}
 
 class ResultHistory(Document):
     history = ListField()
